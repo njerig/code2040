0478779e5d53fbd35f2d5d3b2debe02633a33937
Njeri Gachoka <njerig00@gmail.com>
2016-10-31 03:55:32 -0400

    step 2 temp-complete

--- challenge.py
+++ challenge.py
@@ -10,9 +10,9 @@ def main():
     token = 'a272e0e398eaa14ddeeaaf9cdc21d55d'
     base_url = 'http://challenge.code2040.org/api/'
 
-    step_one(token, base_url+'register')
+    step_one(token, base_url+'register/')
+    step_two(token, base_url+'reverse/')
 
-    #step2
     #step3
     #step4
 
@@ -22,6 +22,17 @@ def step_one(token, url):
 	result = p.text
 	print(result)
 	return result
+
+def step_two(token, url):
+    p_one = requests.post(url, {'token': token})
+
+    the_string = p_one.text
+    r_string = the_string[::-1]
+
+    p_two = requests.post(url+'validate/', {'token': token, 'string': r_string})
+    result = p_two.text
+    print(result)
+    return(result)
 
 if __name__ == '__main__':
     main()
