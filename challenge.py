483647bd2043eeafdc895a80916e2f4193a1a645
Njeri Gachoka <njerig00@gmail.com>
2016-10-31 05:28:02 -0400

    step 4 temp-complete
    but there may be something wrong with it…

--- challenge.py
+++ challenge.py
@@ -10,10 +10,10 @@ def main():
     token = 'a272e0e398eaa14ddeeaaf9cdc21d55d'
     base_url = 'http://challenge.code2040.org/api/'
 
-    step_one(token, base_url+'register/')
-    step_two(token, base_url+'reverse/')
-    step_three(token)
-    #step4
+    #step_one(token, base_url+'register/')
+    #step_two(token, base_url+'reverse/')
+    #step_three(token)
+    step_four(token)
 
 def step_one(token, url):
 	data = {'token': token, 'github': 'https://github.com/njerig/code2040.git'}
@@ -43,6 +43,25 @@ def step_three(token):
         index = haystack.index(needle)
 
     p_two = requests.post('http://challenge.code2040.org/api/haystack/validate', {'token': token, 'needle': index})
+    result = p_two.text
+    print(result)
+    return(result)
+
+def step_four(token):
+    p_one = requests.post('http://challenge.code2040.org/api/prefix', {'token': token})
+    d = ast.literal_eval(p_one.text)
+    print(d)
+    print('\n')
+    array = d['array']
+    prefix = d['prefix']
+    doesnt = []
+    for gibberish in array:
+        if not gibberish.startswith(prefix):
+            doesnt.append(gibberish)
+    print(doesnt)
+    print('\n')
+
+    p_two = requests.post('http://challenge.code2040.org/api/prefix/validate', {'token': token, 'array': doesnt})
     result = p_two.text
     print(result)
     return(result)
