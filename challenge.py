32fb70d3650a79bb14f2c4ced65e6950a4048d3e
Njeri Gachoka <njerig00@gmail.com>
2016-11-01 21:34:07 -0400

    step 4 error
    results in status code 400, “token not found”

--- challenge.py
+++ challenge.py
@@ -4,6 +4,7 @@
 
 import requests
 import ast
+import json
 
 
 def main():
@@ -12,29 +13,29 @@ def main():
 
     #step_one(token, base_url+'register/')
     #step_two(token, base_url+'reverse/')
-    #step_three(token)
-    step_four(token)
+    #step_three(token, base_url+'haystack/')
+    step_four(token, base_url+'prefix/')
+    #step_five(token)
 
 def step_one(token, url):
-	data = {'token': token, 'github': 'https://github.com/njerig/code2040.git'}
-	p = requests.post(url, data)
+	p = requests.post(url, data={'token': token, 'github': 'https://github.com/njerig/code2040.git'})
 	result = p.text
 	print(result)
 	return result
 
 def step_two(token, url):
-    p_one = requests.post(url, {'token': token})
+    p_one = requests.post(url, data={'token': token})
 
     the_string = p_one.text
     r_string = the_string[::-1]
 
-    p_two = requests.post(url+'validate/', {'token': token, 'string': r_string})
+    p_two = requests.post(url+'validate/', data={'token': token, 'string': r_string})
     result = p_two.text
     print(result)
     return(result)
 
-def step_three(token):
-    p_one = requests.post('http://challenge.code2040.org/api/haystack', {'token': token})
+def step_three(token, url):
+    p_one = requests.post(url, data={'token': token})
     d = ast.literal_eval(p_one.text)
 
     needle = d['needle']
@@ -42,29 +43,44 @@ def step_three(token):
     if needle in haystack:
         index = haystack.index(needle)
 
-    p_two = requests.post('http://challenge.code2040.org/api/haystack/validate', {'token': token, 'needle': index})
+    p_two = requests.post(url+'validate/', data={'token': token, 'needle': index})
     result = p_two.text
     print(result)
     return(result)
 
-def step_four(token):
-    p_one = requests.post('http://challenge.code2040.org/api/prefix', {'token': token})
-    d = ast.literal_eval(p_one.text)
+def step_four(token, url):
+    p_one = requests.post(url, {'token': token})
+    '''
+    print(p_one.text)
+    print('\n')
+    '''
+    d = p_one.json()
+    '''
     print(d)
     print('\n')
+    '''
+
     array = d['array']
     prefix = d['prefix']
     doesnt = []
     for gibberish in array:
         if not gibberish.startswith(prefix):
             doesnt.append(gibberish)
+    '''
     print(doesnt)
     print('\n')
+    '''
 
-    p_two = requests.post('http://challenge.code2040.org/api/prefix/validate', {'token': token, 'array': doesnt})
+    p_two = requests.post(url+'validate/', data=json.dumps({'token': token, 'array': doesnt}))
     result = p_two.text
+    print(p_two.status_code)
     print(result)
     return(result)
+
+def step_five(token):
+    p_one = requests.post('http://challenge.code2040.org/api/dating', {'token': token})
+    d = ast.literal_eval(p_one.text)
+    print(d)
 
 if __name__ == '__main__':
     main()
